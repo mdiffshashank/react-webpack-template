4311a1b9e5b32ad87bde515362607cee883c809c
Shashank Trivedi <mdiffshashank@gmail.com>
2024-07-13 09:29:20 +0530

    hot reload added

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,11 @@
+const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 module.exports = {
   mode: "development",
   devtool: "cheap-module-source-map",
+  devserver: {
+    hot: true,
+    open: true,
+  },
+  plugins: [new ReactRefreshPlugin()],
 };
